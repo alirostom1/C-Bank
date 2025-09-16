8b9575ac013b8fac2ffb475e8a6410adbba9ddab
ali-rostom1 <ali.rostom220@gmail.com>
2025-09-16 15:22:46 +0100

    feat : add both checkingsaccounts and savingsaccounts tables scripts

---
+++ src/io/github/alirostom1/cbank/util/tables.sql
@@ -0,0 +1,12 @@
+CREATE TABLE checkingsaccounts(
+    code varchar(36) PRIMARY KEY NOT NULL,
+    balance decimal(10,2),
+    overdraft_limit decimal(10,2),
+    check (balance + overdraft_limit >= 0)
+);
+CREATE TABLE savingsaccounts(
+    code varchar(36) PRIMARY KEY NOT NULL,
+    balance decimal(10,2),
+    interest_rate decimal(1,3),
+    check (interest_rate <= 1 and interest_rate >= 0.01)
+);
