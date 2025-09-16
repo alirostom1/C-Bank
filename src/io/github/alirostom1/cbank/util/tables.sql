c1700061df73d156cb0d31e0f50a42ddd043a4c0
ali-rostom1 <ali.rostom220@gmail.com>
2025-09-16 15:57:59 +0100

    fix : fix accounts tables sql scripts

--- src/io/github/alirostom1/cbank/util/tables.sql
+++ src/io/github/alirostom1/cbank/util/tables.sql
@@ -1,12 +1,17 @@
-CREATE TABLE checkingsaccounts(
+CREATE TABLE accounts(
     code varchar(36) PRIMARY KEY NOT NULL,
-    balance decimal(10,2),
-    overdraft_limit decimal(10,2),
-    check (balance + overdraft_limit >= 0)
+    balance decimal(10,2) DEFAULT 0.00,
+    account_type ENUM("checkings","Savings") DEFAULT "Savings" 
+);
+
+CREATE TABLE checkingsaccounts(
+    code varchar(36) PRIMARY KEY,
+    overdraft_limit decimal(10,2) DEFAULT 500.00,
+    FOREIGN KEY (code) REFERENCES accounts(code)
 );
 CREATE TABLE savingsaccounts(
-    code varchar(36) PRIMARY KEY NOT NULL,
-    balance decimal(10,2),
-    interest_rate decimal(10,3),
-    check (interest_rate <= 1 and interest_rate >= 0.01)
+    code varchar(36) PRIMARY KEY,
+    interest_rate decimal(10,3) default 0.05,
+    check (interest_rate <= 1 and interest_rate >= 0.01),
+    FOREIGN KEY (code) REFERENCES accounts(code)
 );
