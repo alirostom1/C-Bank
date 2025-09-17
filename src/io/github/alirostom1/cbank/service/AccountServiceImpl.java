eda1047f4040bfe74039cd7717e9081d31ec22c1
ali-rostom1 <ali.rostom220@gmail.com>
2025-09-17 11:50:01 +0100

    feat : add account service implementation for business layer

---
+++ src/io/github/alirostom1/cbank/service/AccountServiceImpl.java
@@ -0,0 +1,39 @@
+package io.github.alirostom1.cbank.service;
+
+import io.github.alirostom1.cbank.service.Interface.AccountServiceInterface;
+import io.github.alirostom1.cbank.repository.AccountRepositoryImpl;
+import io.github.alirostom1.cbank.model.entity.SavingsAccount;
+import io.github.alirostom1.cbank.model.entity.CheckingsAccount;
+import io.github.alirostom1.cbank.util.Generator;
+import java.sql.SQLException;
+
+public class AccountServiceImpl implements AccountServiceInterface{
+    private final AccountRepositoryImpl accountRepository; 
+    
+
+    public AccountServiceImpl(){
+        this.accountRepository = new AccountRepositoryImpl();
+    }
+
+    public boolean createAccount(String type){
+        String code = Generator.generateCode();
+        if(type.equals("savings")){
+            SavingsAccount saveAcc = new SavingsAccount(code,0.00);
+            try{
+                accountRepository.store(saveAcc);
+                return true;
+            }catch(SQLException e){
+                return false;
+            }
+        }else if(type.equals("checkings")){
+            CheckingsAccount checkAcc = new CheckingsAccount(code,0.00);
+            try{
+                accountRepository.store(checkAcc);
+                return true;
+            }catch(SQLException e){
+                return false;
+            }
+        }
+        return false;
+    }
+}
