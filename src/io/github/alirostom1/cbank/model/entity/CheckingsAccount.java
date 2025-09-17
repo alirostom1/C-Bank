48089d31d2ee8b4f8139f1b671318e84a381ef14
ali-rostom1 <ali.rostom220@gmail.com>
2025-09-17 09:18:08 +0100

    add CheckingAccount entity

---
+++ src/io/github/alirostom1/cbank/model/entity/CheckingsAccount.java
@@ -0,0 +1,37 @@
+package io.github.alirostom1.cbank.model.entity;
+
+public class CheckingsAccount extends Account{
+    private double overdraftLimit;
+
+    public CheckingsAccount(String code,double balance){
+        super(code,balance);
+        this.overdraftLimit = 500.00;
+    }
+    public CheckingsAccount(String code,double balance,double overdraftLimit){
+        super(code,balance);
+        this.overdraftLimit = overdraftLimit;
+    }
+    public void setOverdraftLimit(double overdraftLimit){
+        this.overdraftLimit = overdraftLimit;
+    }
+    public double getOverdraftLimit(){
+        return this.overdraftLimit;
+    }
+    
+    public boolean withdraw(double amount){
+        if(amount > (overdraftLimit + balance)){
+            return false;
+        }else{
+            balance -= amount;
+            return true;
+        }
+    } 
+    public double calculateInterest(){
+        return 0;
+    }
+    public void displayDetails(){
+        System.out.println("Code : " + code);
+        System.out.println("balance : " + balance);
+        System.out.println("overdraft limit : " + overdraftLimit);
+    }
+}
