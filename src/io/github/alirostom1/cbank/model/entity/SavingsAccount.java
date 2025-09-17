4662411304d53551f77662c586cef22630be5e9b
ali-rostom1 <ali.rostom220@gmail.com>
2025-09-17 09:18:34 +0100

    model/entity/SavingsAccount.java

---
+++ src/io/github/alirostom1/cbank/model/entity/SavingsAccount.java
@@ -0,0 +1,35 @@
+
+package io.github.alirostom1.cbank.model.entity;
+
+public class SavingsAccount extends Account{
+    private double interest = 0.05;
+
+    public SavingsAccount(String code,double balance,double interest){
+        super(code,balance);
+        this.interest = interest;
+    }
+
+    public void setInterest(double interest){
+        this.interest = interest;
+    }
+    public double getInterest(){
+        return this.interest;
+    }
+
+    public boolean withdraw(double amount){
+        if(amount > balance){
+            return false;
+        }else{
+            balance -= amount;
+            return true;
+        }
+    }
+    public double calculateInterest(){
+        return balance * interest;
+    }
+    public void displayDetails(){
+        System.out.println("Code : " + code);
+        System.out.println("balance : " + balance);
+        System.out.println("interest rate : " + interest);
+    }
+}
