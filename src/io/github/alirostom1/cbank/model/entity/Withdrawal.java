7f339c58cf982294529eb25f3c25e1ebb217a136
ali-rostom1 <ali.rostom220@gmail.com>
2025-09-20 09:48:45 +0100

    feat : update Withdrawal class constructor to include code parameter and its accessors

--- src/io/github/alirostom1/cbank/model/entity/Withdrawal.java
+++ src/io/github/alirostom1/cbank/model/entity/Withdrawal.java
@@ -7,8 +7,12 @@ import io.github.alirostom1.cbank.model.enums.Destination;
 public class Withdrawal extends Operation{
     private Destination destination;
 
-    public Withdrawal(UUID id,LocalDateTime date,double amount,Destination destination){
-        super(amount);
+    public Withdrawal(String id,LocalDateTime date,double amount,String code,Destination destination){
+        super(id,date,amount,code);
+        this.destination = destination;
+    }
+    public Withdrawal(double amount,String code,Destination destination){
+        super(amount,code);
         this.destination = destination;
     }
     public Destination getDestination(){
