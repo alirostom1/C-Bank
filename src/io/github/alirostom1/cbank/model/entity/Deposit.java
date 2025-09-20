9fc29e807887b1ae1599468948c934553dd28bc6
ali-rostom1 <ali.rostom220@gmail.com>
2025-09-20 09:48:32 +0100

    feat : update Deposit constructor to include id, date, and code parameters

--- src/io/github/alirostom1/cbank/model/entity/Deposit.java
+++ src/io/github/alirostom1/cbank/model/entity/Deposit.java
@@ -1,12 +1,17 @@
 package io.github.alirostom1.cbank.model.entity;
 
 import io.github.alirostom1.cbank.model.enums.Source;
+import java.time.LocalDateTime;
 
 public class Deposit extends Operation{
     private Source source;
     
-    public Deposit(double amount,Source source){
-        super(amount);
+    public Deposit(String id,LocalDateTime date,double amount,String code,Source source){
+        super(id,date,amount,code);
+        this.source = source;
+    }
+    public Deposit(double amount,String code,Source source){
+        super(amount,code);
         this.source = source;
     }
     public Source getSource(){
